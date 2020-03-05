d3504c0a77aa13c7cf9747664626d40a8d7ecadc
Xin Qian <last.qx@gmail.com>
2020-03-04 19:33:32 -0500

    update

--- inc/WCPPID/ProtoSegment.h
+++ inc/WCPPID/ProtoSegment.h
@@ -6,11 +6,26 @@
 namespace WCPPID{
   class ProtoSegment{
   public:
-    ProtoSegment();
+    ProtoSegment(std::list<WCP::WCPointCloud<double>::WCPoint >& path_wcps );
     ~ProtoSegment();
+
+    std::vector<WCP::WCPointCloud<double>::WCPoint >& get_wcpt_vec(){return wcpt_vec;};
+    std::vector<WCP::Point >& get_pt_vec(){return fit_pt_vec;};
+    void clear_pt_vec(){fit_pt_vec.clear();};
+
+    void set_fit_pt_vec(std::vector<WCP::Point >& tmp_fit_pt_vec){fit_pt_vec = tmp_fit_pt_vec;};
+    
+    bool get_fit_flag(){return flag_fit;};
+    void set_fit_flag(bool flag){flag_fit = flag;};
+    
   protected:
-    
-    
+    std::vector<WCP::WCPointCloud<double>::WCPoint > wcpt_vec;
+    std::vector<WCP::Point > fit_pt_vec;
+
+    ProtoVertex *v1;
+    ProtoVertex *v2;
+
+    bool flag_fit;
   };
   typedef std::vector<ProtoSegment*> ProtoSegmentSelection;
 }
--- src/ProtoSegment.cxx
+++ src/ProtoSegment.cxx
@@ -2,8 +2,20 @@
 
 using namespace WCP;
 
-WCPPID::ProtoSegment::ProtoSegment(){
+WCPPID::ProtoSegment::ProtoSegment(std::list<WCP::WCPointCloud<double>::WCPoint >& path_wcps )
+  : flag_fit(false)
+{
+  v1 = new ProtoVertex(path_wcps.front() );
+  v2 = new ProtoVertex(path_wcps.back() );
+
+  for (auto it = path_wcps.begin(); it!=path_wcps.end(); it++){
+    wcpt_vec.push_back(*it);
+    Point p(it->x, it->y, it->z);
+    fit_pt_vec.push_back(p);
+  }
 }
 
 WCPPID::ProtoSegment::~ProtoSegment(){
+  delete v1;
+  delete v2;
 }
