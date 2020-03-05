6114a40468ab5c3e1fdfe9886220e9c7ab483b16
Xin Qian <last.qx@gmail.com>
2020-03-05 09:05:28 -0500

    update code

--- inc/WCPPID/ProtoSegment.h
+++ inc/WCPPID/ProtoSegment.h
@@ -2,6 +2,7 @@
 #define WCPPID_PROTOSEGMENT_H
 
 #include "WCPPID/ProtoVertex.h"
+#include "WCPData/ToyPointCloud.h"
 
 namespace WCPPID{
   class ProtoSegment{
@@ -10,15 +11,19 @@ namespace WCPPID{
     ~ProtoSegment();
 
     std::vector<WCP::WCPointCloud<double>::WCPoint >& get_wcpt_vec(){return wcpt_vec;};
-    std::vector<WCP::Point >& get_pt_vec(){return fit_pt_vec;};
+    std::vector<WCP::Point >& get_point_vec(){return fit_pt_vec;};
     std::vector<double>& get_dQ_vec(){return dQ_vec;};
     std::vector<double>& get_dx_vec(){return dx_vec;};
-
-    std::vector<double> get_dQ_dx_vec(){return dQ_dx_vec;};
+    std::vector<double>& get_dQ_dx_vec(){return dQ_dx_vec;};
+    std::vector<double>& get_pu_vec(){return pu_vec;};
+    std::vector<double>& get_pv_vec(){return pv_vec;};
+    std::vector<double>& get_pw_vec(){return pw_vec;};
+    std::vector<double>& get_pt_vec(){return pt_vec;};
+    std::vector<double>& get_reduced_chi2_vec(){return reduced_chi2_vec;};
     
     // after fit
     void clear_fit();
-    void set_fit_vec(std::vector<WCP::Point >& tmp_fit_pt_vec, std::vector<double>& tmp_dQ_vec, std::vector<double>& tmp_dx_vec);
+    void set_fit_vec(std::vector<WCP::Point >& tmp_fit_pt_vec, std::vector<double>& tmp_dQ_vec, std::vector<double>& tmp_dx_vec, std::vector<double>& tmp_pu_vec, std::vector<double>& tmp_pv_vec, std::vector<double>& tmp_pw_vec, std::vector<double>& tmp_pt_vec, std::vector<double>& tmp_reduced_chi2_vec);
     
 
     // if fit_flag is set, the fit_pts are useful ...
@@ -32,6 +37,14 @@ namespace WCPPID{
     std::vector<double> dQ_vec;
     std::vector<double> dx_vec;
     std::vector<double> dQ_dx_vec;
+    std::vector<double> pu_vec;
+    std::vector<double> pv_vec;
+    std::vector<double> pw_vec;
+    std::vector<double> pt_vec;
+    std::vector<double> reduced_chi2_vec;
+
+    // point cloud ...
+    WCP::ToyPointCloud* pcloud_fit;
     
     bool flag_fit;
   };
--- src/ProtoSegment.cxx
+++ src/ProtoSegment.cxx
@@ -4,6 +4,7 @@ using namespace WCP;
 
 WCPPID::ProtoSegment::ProtoSegment(std::list<WCP::WCPointCloud<double>::WCPoint >& path_wcps )
   : flag_fit(false)
+  , pcloud_fit(0)
 {
   
   for (auto it = path_wcps.begin(); it!=path_wcps.end(); it++){
@@ -13,11 +14,13 @@ WCPPID::ProtoSegment::ProtoSegment(std::list<WCP::WCPointCloud<double>::WCPoint 
 }
 
 WCPPID::ProtoSegment::~ProtoSegment(){
+  if (pcloud_fit != (ToyPointCloud*)0)
+    delete pcloud_fit;
 }
 
 
 
-void WCPPID::ProtoSegment::set_fit_vec(std::vector<WCP::Point >& tmp_fit_pt_vec, std::vector<double>& tmp_dQ_vec, std::vector<double>& tmp_dx_vec){
+void WCPPID::ProtoSegment::set_fit_vec(std::vector<WCP::Point >& tmp_fit_pt_vec, std::vector<double>& tmp_dQ_vec, std::vector<double>& tmp_dx_vec, std::vector<double>& tmp_pu_vec, std::vector<double>& tmp_pv_vec, std::vector<double>& tmp_pw_vec, std::vector<double>& tmp_pt_vec, std::vector<double>& tmp_reduced_chi2_vec){
   flag_fit = true;
   
   fit_pt_vec = tmp_fit_pt_vec;
@@ -27,6 +30,17 @@ void WCPPID::ProtoSegment::set_fit_vec(std::vector<WCP::Point >& tmp_fit_pt_vec,
   for (size_t i=0;i!=dQ_vec.size();i++){
     dQ_dx_vec.at(i) = dQ_vec.at(i)/(dx_vec.at(i)+1e-9);
   }
+
+  pu_vec = tmp_pu_vec;
+  pv_vec = tmp_pv_vec;
+  pw_vec = tmp_pw_vec;
+  pt_vec = tmp_pt_vec;
+  reduced_chi2_vec = tmp_reduced_chi2_vec;
+
+  if (pcloud_fit != (ToyPointCloud*)0) delete pcloud_fit;
+  pcloud_fit = new ToyPointCloud();
+  pcloud_fit->AddPoints(fit_pt_vec);
+  pcloud_fit->build_kdtree_index();
 }
 
 void WCPPID::ProtoSegment::clear_fit(){
@@ -35,4 +49,11 @@ void WCPPID::ProtoSegment::clear_fit(){
   dQ_vec.clear();
   dx_vec.clear();
   dQ_dx_vec.clear();
+
+  pu_vec.clear();
+  pv_vec.clear();
+  pw_vec.clear();
+  pt_vec.clear();
+  reduced_chi2_vec.clear();
+  
 }
