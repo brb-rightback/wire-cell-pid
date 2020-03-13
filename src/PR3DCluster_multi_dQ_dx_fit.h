e2fffa2d7e194129480abd59d2c7b2365bb6ab0c
Xin Qian <last.qx@gmail.com>
2020-03-13 09:45:17 -0400

    update

--- src/PR3DCluster_multi_dQ_dx_fit.h
+++ src/PR3DCluster_multi_dQ_dx_fit.h
@@ -356,6 +356,7 @@ void WCPPID::PR3DCluster::dQ_dx_multi_fit(std::map<WCPPID::ProtoVertex*, WCPPID:
   for (auto it = map_vertex_segments.begin(); it!= map_vertex_segments.end(); it++){
     WCPPID::ProtoVertex *vtx = it->first;
     Point curr_rec_pos = vtx->get_fit_pt();
+    int index = vtx->get_fit_index();
     PointVector connected_pts;
     for (auto it1 = it->second.begin(); it1!=it->second.end(); it1++){
       WCPPID::ProtoSegment *sg = *it1;
@@ -406,7 +407,144 @@ void WCPPID::PR3DCluster::dQ_dx_multi_fit(std::map<WCPPID::ProtoVertex*, WCPPID:
     /*   WCPPID::ProtoSegment *sg = *it1; */
     /*   std::cout << vtx->get_dx() << " " << sg->get_dx_vec().front() << " " << sg->get_dx_vec().back() << " " << std::endl; */
     /* } */
+
+    std::vector<double> centers_U ;
+    std::vector<double> centers_V ;
+    std::vector<double> centers_W ;
+    std::vector<double> centers_T ;
+    std::vector<double> sigmas_T;
+    std::vector<double> sigmas_U;
+    std::vector<double> sigmas_V;
+    std::vector<double> sigmas_W;
+    std::vector<double> weights;
+
+    for (int k=0;k!=connected_pts.size();k++){
+      for (int j=0;j!=5;j++){
+	Point reco_pos;
+	reco_pos.x = connected_pts.at(k).x + (curr_rec_pos.x - connected_pts.at(k).x)/5.*(j+0.5);
+	reco_pos.y = connected_pts.at(k).y + (curr_rec_pos.y - connected_pts.at(k).y)/5.*(j+0.5);
+	reco_pos.z = connected_pts.at(k).z + (curr_rec_pos.z - connected_pts.at(k).z)/5.*(j+0.5);
+	double central_T = offset_t + slope_xt * reco_pos.x ;
+	double central_U = offset_u + (slope_yu * reco_pos.y + slope_zu * reco_pos.z);
+	double central_V = offset_v + (slope_yv * reco_pos.y + slope_zv * reco_pos.z)+2400;
+	double central_W = offset_w + (slope_yw * reco_pos.y + slope_zw * reco_pos.z)+4800;
+	double weight = sqrt(pow(connected_pts.at(k).x-curr_rec_pos.x,2)+
+			     pow(connected_pts.at(k).y-curr_rec_pos.y,2)+
+			     pow(connected_pts.at(k).z-curr_rec_pos.z,2));
+	double drift_time = reco_pos.x/time_slice_width * nrebin * 0.5*units::microsecond  - flash_time ;
+	if (drift_time <50 *units::microsecond ) drift_time = 50* units::microsecond;
+      
+	//  std::cout << drift_time/units::microsecond << std::endl;
+	double diff_sigma_L = sqrt(2* DL * drift_time);
+	double diff_sigma_T = sqrt(2* DT * drift_time);
+
+	double sigma_L = sqrt(pow(diff_sigma_L,2) + pow(add_sigma_L,2))/time_slice_width;
+	double sigma_T_u = sqrt(pow(diff_sigma_T,2) + pow(ind_sigma_u_T,2))/pitch_u;
+	double sigma_T_v = sqrt(pow(diff_sigma_T,2) + pow(ind_sigma_v_T,2))/pitch_v;
+	double sigma_T_w = sqrt(pow(diff_sigma_T,2) + pow(col_sigma_w_T,2))/pitch_w;
+
+	centers_U.push_back(central_U);
+	centers_V.push_back(central_V);
+	centers_W.push_back(central_W);
+	centers_T.push_back(central_T);
+	
+	weights.push_back(weight);
+	
+	sigmas_U.push_back(sigma_T_u);
+	sigmas_V.push_back(sigma_T_v);
+	sigmas_W.push_back(sigma_T_w);
+	sigmas_T.push_back(sigma_L);
+      }
+    }
     
+    int n_u = 0;
+    double sum_u = 0;
+    for (auto it = map_2D_ut_charge.begin(); it!= map_2D_ut_charge.end(); it++){
+      if (fabs(it->first.first - centers_U.front()) <= 10 && fabs(it->first.second - centers_T.front()) <= 10 ){
+	double value = cal_gaus_integral_seg(it->first.second, it->first.first,centers_T, sigmas_T, centers_U, sigmas_U, weights , 0 , 4);
+	sum_u += value;
+	// near dead channels ...
+	if (std::get<2>(it->second)==0 && value > 0) reg_flag_u.at(index) = 1;
+	
+	if (value > 0 && std::get<0>(it->second) >0 && std::get<2>(it->second)!=0){
+	  // if (i!=143)
+	  RU.insert(n_u,index) = value/sqrt(pow(std::get<1>(it->second),2)+pow(std::get<0>(it->second)*rel_uncer_ind,2) + pow(add_uncer_ind,2));
+	  // if (i==143) std::cout << "U: " << it->first.first << " " << it->first.second << " " << value << std::endl;
+	}
+      }
+      n_u ++;
+    }
+
+    int n_v = 0;
+    double sum_v = 0;
+    for (auto it = map_2D_vt_charge.begin(); it!= map_2D_vt_charge.end(); it++){
+      if (fabs(it->first.first +  2400 - centers_V.front()) <= 10 && fabs(it->first.second - centers_T.front()) <= 10 ){
+	double value = cal_gaus_integral_seg(it->first.second, it->first.first + 2400, centers_T, sigmas_T, centers_V, sigmas_V, weights , 0 , 4);
+	sum_v += value;
+	// near dead channels
+	if (std::get<2>(it->second)==0 && value > 0) {
+	  // if (i==136) std::cout << it->first.first << " " << it->first.second << std::endl;
+	  reg_flag_v.at(index) = 1;
+	}
+
+	
+	if (value > 0 && std::get<0>(it->second) >0 && std::get<2>(it->second)!=0){
+	  //	  if (i==136) std::cout << "V: " << it->first.first << " " << it->first.second << " " << value << std::endl;
+	  // if (i!=143)
+	  RV.insert(n_v,index) = value/sqrt(pow(std::get<1>(it->second),2)+pow(std::get<0>(it->second)*rel_uncer_ind,2) + pow(add_uncer_ind,2));
+	}
+      }
+      n_v ++;
+    }
+
+    
+    int n_w = 0;
+    double sum_w = 0;
+    for (auto it = map_2D_wt_charge.begin(); it!= map_2D_wt_charge.end(); it++){
+      if (fabs(it->first.first + 4800 - centers_W.front()) <= 10 &&
+      	  fabs(it->first.second - centers_T.front()) <= 10 ){
+	double value = cal_gaus_integral_seg(it->first.second, it->first.first + 4800,centers_T, sigmas_T, centers_W, sigmas_W, weights , 0 , 4);
+	sum_w += value;
+	// near dead channels ...
+	if (std::get<2>(it->second)==0 && value > 0) reg_flag_w.at(index) = 1;
+	
+	// relevant && charge > 0 && not dead channel ...
+	if (value > 0 && std::get<0>(it->second) >0 && std::get<2>(it->second)!=0){
+	  // if (i!=143)
+	  RW.insert(n_w,index) = value/sqrt(pow(std::get<1>(it->second),2)+pow(std::get<0>(it->second)*rel_uncer_col,2) + pow(add_uncer_col,2));
+	  //  if (i==147) std::cout << "W: " << it->first.first << " " << it->first.second << " " << value << std::endl;
+	}
+      }
+      n_w ++;
+    }
+
+    // additional checks on the dead channels ...
+    if (reg_flag_u.at(index)==0){
+      for (size_t kk = 0; kk!=centers_U.size(); kk++){
+    	if (map_2D_ut_charge.find(std::make_pair(int(centers_U.at(kk)), int(centers_T.at(kk)) ))==map_2D_ut_charge.end()) {
+    	  reg_flag_u.at(index) =1;
+    	  break;
+    	}
+      }
+    }
+    if (reg_flag_v.at(index)==0){
+      for (size_t kk = 0; kk!=centers_V.size(); kk++){
+    	if (map_2D_vt_charge.find(std::make_pair(int(centers_V.at(kk)-2400), int(centers_T.at(kk)) ))==map_2D_vt_charge.end()) {
+	  //if (i==136)  std::cout << centers_V.at(kk) << " " << centers_T.at(kk) << std::endl;
+    	  reg_flag_v.at(index) =1;
+    	  break;
+    	}
+      }
+    }
+    if (reg_flag_w.at(index)==0){
+      for (size_t kk = 0; kk!=centers_W.size(); kk++){
+    	if (map_2D_wt_charge.find(std::make_pair(int(centers_W.at(kk)-4800), int(centers_T.at(kk)) ))==map_2D_wt_charge.end()) {
+    	  reg_flag_w.at(index) =1;
+    	  break;
+    	}
+      }
+    }
+
     
     
   }
